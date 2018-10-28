6f04ffbdaf4a4928040fab3177605e812fcc5a2a
Danny Robinson <daniel@djrobinson.me>
2018-10-28 13:17:37 -0600

    New approach

--- trade-engine/actions/index.ts
+++ trade-engine/actions/index.ts
@@ -5,18 +5,7 @@ export const addTodo = text => ({
   text
 })
 
-export const setVisibilityFilter = filter => ({
-  type: 'SET_VISIBILITY_FILTER',
-  filter
-})
-
 export const toggleTodo = id => ({
   type: 'TOGGLE_TODO',
   id
 })
-
-export const VisibilityFilters = {
-  SHOW_ALL: 'SHOW_ALL',
-  SHOW_COMPLETED: 'SHOW_COMPLETED',
-  SHOW_ACTIVE: 'SHOW_ACTIVE'
-}
--- trade-engine/store/configureStore.ts
+++ trade-engine/store/configureStore.ts
@@ -1,12 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import api from '../middleware/api'
+import saga from 'redux-saga'
 import rootReducer from '../reducers'
 
 const configureStore = preloadedState => createStore(
   rootReducer,
-  preloadedState,
-  applyMiddleware(thunk, api)
+  preloadedState
 )
 
 export default configureStore
