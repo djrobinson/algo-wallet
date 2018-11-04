761e0f1c889f5d12bff62034ca443021af263c1d
Danny Robinson <daniel@djrobinson.me>
2018-11-04 12:51:20 -0700

    Basic balances styling

--- client/src/balances/balances-tile/BalancesTile.jsx
+++ client/src/balances/balances-tile/BalancesTile.jsx
@@ -3,13 +3,26 @@ import { Col, Row } from 'react-bootstrap';
 import './BalancesTile.css';
 
 class BalancesTile extends Component {
+
+
   render() {
     return (
       <div className="balances-tile-container">
         <h1>{this.props.mkt}</h1>
-        <span className="total">{this.props.balances ? this.props.balances.bittrex.total : ''}</span>
-        <span className="used">{this.props.balances ? this.props.balances.bittrex.used : ''}</span>
-        <span className="free">{this.props.balances ? this.props.balances.bittrex.free : '' }</span>
+        <div className="exchanges">
+          <div className="exchange-balance-container">
+            <h5>Bittrex</h5>
+            <span className="total">{this.props.balances.bittrex ? this.props.balances.bittrex.total : 0.00}</span>
+            <span className="used">{this.props.balances.bittrex ? this.props.balances.bittrex.used : 0.00}</span>
+            <span className="free">{this.props.balances.bittrex ? this.props.balances.bittrex.free : 0.00 }</span>
+          </div>
+          <div className="exchange-balance-container">
+            <h5>Poloniex</h5>
+            <span className="total">{this.props.balances.poloniex ? this.props.balances.poloniex.total : 0.00}</span>
+            <span className="used">{this.props.balances.poloniex ? this.props.balances.poloniex.used : 0.00}</span>
+            <span className="free">{this.props.balances.poloniex ? this.props.balances.poloniex.free : 0.00}</span>
+          </div>
+        </div>
       </div>
     );
   }
